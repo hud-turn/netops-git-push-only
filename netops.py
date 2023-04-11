6b9a555a39e870bf4519fac0fb7d0f27344eb3cf
Hudson Turner <30182083+hturner80631@users.noreply.github.com>
2023-04-11 01:55:16 -0600

    Reconfiguring things

--- netops.py
+++ netops.py
@@ -3,19 +3,18 @@ from dotenv import load_dotenv
 
 
 ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-load_dotenv()
-
 with open('targets.csv', 'r') as csvfile:
     reader = csv.reader(csvfile)
     for row in reader:
         ipaddress = row[0]
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        load_dotenv()
         ssh.connect(ipaddress, username= os.getenv('USERNAME'), password= os.getenv('PASSWORD'))
         stdin, stderr = ssh.exec_command('')
         stdin, stderr = ssh.exec_command('en')
         stdin, stderr = ssh.exec_command('terminal length 0')
         stdin, stdout, stderr = ssh.exec_command('show running-config')
         output = stdout.readlines()
-        with open('output.txt', 'w') as f:
+        with open(f'{ipaddress}.txt', 'r') as f:
             f.writelines(output)
         ssh.close()
