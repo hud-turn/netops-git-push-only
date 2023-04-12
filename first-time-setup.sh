8aa4458471eb9498d5f8ed0e695a5fdcf7b7d272
Hudson Turner <30182083+hturner80631@users.noreply.github.com>
2023-04-12 14:49:38 -0600

    Updating install script

--- first-time-setup.sh
+++ first-time-setup.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-sudo apt-get install python3
-sudo apt-get install python3-pip
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install python3 -y
+sudo apt-get install python3-pip -y
 sudo pip install --upgrade pip
-sudo pip install python-dotenv
-sudo pip install paramiko
+sudo pip install python-dotenv -y
+sudo pip install paramiko -y
