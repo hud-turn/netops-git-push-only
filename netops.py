42739bd6718e3c84c41da0712659ba6ecff9ef7c
Hudson Turner <30182083+hturner80631@users.noreply.github.com>
2023-04-13 23:45:42 -0600

    Commits working

--- netops.py
+++ netops.py
@@ -1,7 +1,9 @@
-import paramiko, requests, json, os, csv, datetime
+import paramiko, requests, json, os, csv, requests, base64
 from dotenv import load_dotenv
 from github import Github
-load_dotenv()
+from datetime import datetime
+from requests.auth import HTTPBasicAuth
+
 
 #Directory Operations
 directory_path = os.getcwd()
@@ -19,6 +21,9 @@ def write_list_to_file(lst, filename):
 
 #Importing Paramiko
 ssh = paramiko.SSHClient()
+
+#Loading envs
+load_dotenv()
 
 #Defining SSH commands
 cmd0 = "terminal length 0"
@@ -42,20 +47,63 @@ with open('targets.csv', newline='') as csvfile:
                 ssh.close()
             except:
                 print("Unable to establish SSH connection with " + ipaddress)
-    try:
-        #Importing Credentials and Information
-        g = Github(os.getenv('GITHUBACCESSTOKEN'))
-        user = g.get_user()
-        repo = user.get_repo(os.getenv('GITHUBREPO'))
-        dir_path = subdirectory_path
-        commit_message = datetime.date.now().strftime("%I:%M%p on %B %d, %Y")
-        # Loop through all files in the current directory:
-        for filename in os.listdir(dir_path):
-            # Open the file:
-            with open(os.path.join(dir_path, filename), 'r') as file:
-                content = file.read()
-            # Create a new file in the repository:
-                repo.create_file(filename, commit_message, content)
-    except:
-        print("Unable to connect to Github")
+        # Replace the directory path with the path to your directory
+        directory_path = subdirectory_path
+        # Replace the repository owner with your username or organization name
+        repository_owner = os.getenv('GITHUBREPOOWNER')
+        # Replace the repository name with your repository name
+        repository_name = os.getenv('GITHUBREPONAME')
+        # Replace the branch name with your branch name
+        branch_name = os.getenv('BRANCH')
+        # Replace the commit message with your commit message
+        commit_message = ("test")
+        ##print(type(datetime.now.strftime("%I:%M%p on %B %d, %Y")))
+        # Replace the GitHub username with your username
+        github_username = os.getenv('GITHUBUSERNAME')
+        # Replace the GitHub personal access token with your personal access token
+        github_token = os.getenv('GITHUBACCESSTOKEN')
+        # Get the SHA of the latest commit on the branch
+        url = f"https://api.github.com/repos/{repository_owner}/{repository_name}/git/refs/heads/{branch_name}"
+        response = requests.get(url, auth=HTTPBasicAuth(github_username, github_token))
+        sha = response.json()["object"]["sha"]
+        # Create a new tree object with the files in the directory
+        tree_url = f"https://api.github.com/repos/{repository_owner}/{repository_name}/git/trees"
+        tree_data = {
+            "base_tree": sha,
+            "tree": []
+        }
+        for root, dirs, files in os.walk(directory_path):
+            for file in files:
+                file_path = os.path.join(root, file)
+                with open(file_path, "rb") as f:
+                    file_content = f.read()
+                    file_content_base64 = base64.b64encode(file_content).decode("utf-8")
+                    tree_data["tree"].append({
+                    "path": os.path.relpath(file_path, directory_path),
+                    "mode": "100644",
+                    "type": "blob",
+                    "content": file_content_base64
+                })
+        response = requests.post(tree_url, json=tree_data, auth=HTTPBasicAuth(github_username, github_token))
+        tree_sha = response.json()["sha"]
+        print("here")
+        # Create a new commit object with the new tree object
+        commit_url = f"https://api.github.com/repos/{repository_owner}/{repository_name}/git/commits"
+        commit_data = {
+            "message": commit_message,
+            "parents": [sha],
+            "tree": tree_sha
+        }
+        response = requests.post(commit_url, json=commit_data, auth=HTTPBasicAuth(github_username, github_token))
+        commit_sha = response.json()["sha"]
+        print("here1")
+        # Update the branch reference to point to the new commit object
+        ref_url = f"https://api.github.com/repos/{repository_owner}/{repository_name}/git/refs/heads/{branch_name}"
+        ref_data = {
+            "sha": commit_sha,
+            "force": True
+        }
+        response = requests.patch(ref_url, json=ref_data, auth=HTTPBasicAuth(github_username, github_token))
+        print("Files pushed to GitHub successfully!")
+
     
