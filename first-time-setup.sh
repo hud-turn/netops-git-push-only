02478cf0119d03100c08453fec2ff285058fd71f
hud-turn <github.number821@passinbox.com>
2025-08-31 11:47:41 -0600

    Cleaning up repo

--- first-time-setup.sh
+++ first-time-setup.sh
@@ -4,8 +4,6 @@ sudo apt-get upgrade -y
 sudo apt-get install python3 -y
 sudo apt-get install python3-pip -y
 sudo pip install --upgrade pip
-sudo pip install python-dotenv -y
-sudo pip install paramiko -y
-sudo pip install PyGithub -y
-sudo apt-get install git-all -y
+sudo pip install python3-venv -y
+sudo apt-get install git -y
 
